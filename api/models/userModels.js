e48bce68306a272ad8dca7209100647e0294e970
Justin Hong <jhong2009@gmail.com>
2018-05-18 09:24:13 -0700

    in userModel.js: set up checkPassword method to compare passwords

--- api/models/userModels.js
+++ api/models/userModels.js
@@ -26,10 +26,10 @@ UserSchema.pre("save", function(next) {
 });
 
 UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
-  // https://github.com/kelektiv/node.bcrypt.js#usage
-  // Fill this method in with the Proper password comparing, bcrypt.compare()
-  // Your controller will be responsible for sending the information here for password comparison
-  // Once you have the user, you'll need to pass the encrypted pw and the plaintext pw to the compare function
+  bcrypt.compare(plainTextPW, this.password, (err, isMatch) => {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
 };
 
 module.exports = mongoose.model("User", UserSchema);
