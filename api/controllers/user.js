e99ee7e5bdbecd3097e91c5fedf384a2f4348f60
Justin Hong <jhong2009@gmail.com>
2018-05-18 10:31:05 -0700

    in user.js: set up createUser() to create a new User instance and call save()

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,12 +1,18 @@
-const User = require('../models/userModels');
-const bcrypt = require('bcrypt');
+const User = require("../models/userModels");
+const bcrypt = require("bcrypt");
 
 const createUser = (req, res) => {
-  const { username, password } = req.body;
-  // create user takes in the username and password and saves a user.
-  // our pre save hook should kick in here saving this user to the DB with an encrypted password.
+	const { username, password } = req.body;
+	// create user takes in the username and password and saves a user.
+	// our pre save hook should kick in here saving this user to the DB with an encrypted password.
+	const user = new User({ username, password });
+
+	user.save((err, user) => {
+		if (err) return res.send(err);
+		res.json(user);
+	});
 };
 
 module.exports = {
-  createUser
+	createUser
 };
